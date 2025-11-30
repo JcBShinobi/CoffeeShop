a172316c924ff59a7465de685c1725ed691f672b
JcBShinobi <rava.santiago@gmail.com>
2025-11-30 19:13:04 +0000

    \

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// TODO: replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAZ-t06eMaiog0b7abqqr_lavqmtOd5NwQ",
+  authDomain: "coffeeshop-54f57.firebaseapp.com",
+  projectId: "coffeeshop-54f57",
+  storageBucket: "coffeeshop-54f57.firebasestorage.app",
+  messagingSenderId: "433235862666",
+  appId: "1:433235862666:web:333b9e93e45ffce08cb973",
+  measurementId: "G-F5RCNQ35QC"
 };
 
 const app = initializeApp(firebaseConfig);
