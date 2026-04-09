5fb0f88db3573119d3165ec3e52257162358ef33
JcBShinobi <rava.santiago@gmail.com>
2026-04-09 21:21:43 +0100

    Setup automatic Firebase deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,13 +11,13 @@ import {
 } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ-t06eMaiog0b7abqqr_lavqmtOd5NwQ",
-  authDomain: "coffeeshop-54f57.firebaseapp.com",
-  projectId: "coffeeshop-54f57",
-  storageBucket: "coffeeshop-54f57.firebasestorage.app",
-  messagingSenderId: "433235862666",
-  appId: "1:433235862666:web:333b9e93e45ffce08cb973",
-  measurementId: "G-F5RCNQ35QC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
